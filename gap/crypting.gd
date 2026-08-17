4a87005d42691d9fb73a61744f3185cec0a08960
Max Horn <max@quendi.de>
2026-08-18 00:37:49 +0200

    Don't depend on GAPDoc just for RepeatedString (#26)

--- gap/crypting.gd
+++ gap/crypting.gd
@@ -17,13 +17,6 @@ DeclareCategory("IsSHA256State", IsObject);
 
 #! @Description
 #!   Call <Ref Label="HexStringInt" BookName="ref"/> on the argument <A>int</A>
-#!   then pad the string on the left to <A>length</A> using padding
-#!   letter <A>pad</A>
-#! @Arguments int, pad, length
-DeclareGlobalFunction("CRYPTING_HexStringIntPad");
-
-#! @Description
-#!   Call <Ref Label="HexStringInt" BookName="ref"/> on the argument <A>int</A>
 #!   then pad the string on the left to length 8 using padding
 #!   letter 0.
 #! @Arguments int
